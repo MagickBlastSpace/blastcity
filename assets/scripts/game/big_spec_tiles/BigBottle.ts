b6f39a3af4b49e9e3d6a09ea2f9b09b2e8459d2c
BlindBandit <georgynosar@gmail.com>
2024-01-11 21:07:56 +0700

    Fixed Bottle bug

--- assets/scripts/game/big_spec_tiles/BigBottle.ts
+++ assets/scripts/game/big_spec_tiles/BigBottle.ts
@@ -4,6 +4,9 @@ const { ccclass, property } = _decorator;
 
 @ccclass('BigBottle')
 export class BigBottle extends BigTileBase {
+
+    private lowestRow: number = 0;
+
     init(row: number, col: number, tileType: string) {
         super.init(row, col, tileType);
 
@@ -14,7 +17,7 @@ export class BigBottle extends BigTileBase {
 
 
     isReadyToDestroy(): boolean {
-        if(this.row === 0) {
+        if(this.row === this.lowestRow) {
             return true;
         }
         return false;
@@ -22,6 +25,41 @@ export class BigBottle extends BigTileBase {
 
     startDestroyConsequences() {
         this.node.emit("goal", "bottle");
+    }
+
+
+    subscribeOnFieldEvents(field: Node) {
+        if(this.isSubscribed) {
+            return;
+        }
+        
+        super.subscribeOnFieldEvents(field);
+
+        let fieldComp = field.getComponent("Field");
+        
+        this.lowestRow = this.findLowestRow(fieldComp.getTilesArray());
+    }
+
+
+    findLowestRow(field: Node[][]): number {
+        if(this.row === 0) {
+            return 0;
+        }
+
+        let lowestRow = this.row;
+        for(let i = this.row - 1; i >= 0; i--) {
+            const tile = field[i][this.col];
+            const tile2 = field[i][this.col + 1];
+            if(tile !== null && tile !== undefined && tile2 !== null && tile2 !== undefined) {
+                const tileComp = tile.getComponent("TileBase");
+                const tileComp2 = tile.getComponent("TileBase");
+                if(!tileComp.isEmptyTile() && !tileComp2.isEmptyTile()) {
+                    lowestRow = tileComp.getRow();
+                }
+            }
+        }
+
+        return lowestRow;
     }
 }
 
--- assets/scripts/game/spec_tiles/lvl_9/Bottle.ts
+++ assets/scripts/game/spec_tiles/lvl_9/Bottle.ts
@@ -4,6 +4,9 @@ const { ccclass, property } = _decorator;
 
 @ccclass('Bottle')
 export class Bottle extends SpecTileBase {
+
+    private lowestRow: number = 0;
+
     init(row: number, col: number, tileType: string) {
         super.init(row, col, tileType);
 
@@ -12,7 +15,7 @@ export class Bottle extends SpecTileBase {
 
 
     isReadyToDestroy(): boolean {
-        if(this.row === 0) {
+        if(this.row === this.lowestRow) {
             return true;
         }
         return false;
@@ -20,6 +23,39 @@ export class Bottle extends SpecTileBase {
 
     startDestroyConsequences() {
         this.node.emit("goal", "bottle");
+    }
+
+
+    subscribeOnFieldEvents(field: Node) {
+        if(this.isSubscribed) {
+            return;
+        }
+        
+        super.subscribeOnFieldEvents(field);
+
+        let fieldComp = field.getComponent("Field");
+        
+        this.lowestRow = this.findLowestRow(fieldComp.getTilesArray());
+    }
+
+
+    findLowestRow(field: Node[][]): number {
+        if(this.row === 0) {
+            return 0;
+        }
+
+        let lowestRow = this.row;
+        for(let i = this.row - 1; i >= 0; i--) {
+            const tile = field[i][this.col];
+            if(tile !== null && tile !== undefined) {
+                const tileComp = tile.getComponent("TileBase");
+                if(!tileComp.isEmptyTile()) {
+                    lowestRow = tileComp.getRow();
+                }
+            }
+        }
+
+        return lowestRow;
     }
 }
 
