05aba5aa8207eed7b323ea0640958ea91d52b339
BlindBandit <georgynosar@gmail.com>
2024-02-26 15:48:01 +0700

    Fixed boosters when click on status with null tile

--- assets/scripts/game/statuses/StatusBase.ts
+++ assets/scripts/game/statuses/StatusBase.ts
@@ -19,6 +19,15 @@ export class StatusBase extends Component {
     private currentTween: any = null;
 
     private isSubscribed: boolean = false;
+
+
+    onLoad() {
+        this.node.on(Node.EventType.TOUCH_START, this.onTouchStart, this);
+    }
+
+    onDestroy() {
+        this.node.off(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
+    }
     
     
     init(row: number, col: number, statusType: string) {
@@ -117,6 +126,11 @@ export class StatusBase extends Component {
 
     getCustomParameter(): number {
         return 0;
+    }
+
+
+    onTouchStart(event: cc.Event.EventTouch) {
+        this.node.emit("click", this.node);
     }
 }
 
