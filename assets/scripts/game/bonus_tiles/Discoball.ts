ac9267fc9419bf8f414c57b87e4196f59faf9cd4
BlindBandit <georgynosar@gmail.com>
2024-03-26 15:48:09 +0700

    Fixed discoball combo doubled respawn

--- assets/scripts/game/bonus_tiles/Discoball.ts
+++ assets/scripts/game/bonus_tiles/Discoball.ts
@@ -178,7 +178,7 @@ export class Discoball extends BonusTileBase {
             }
         }
 
-        this.setRespawnEvent(this.respawnDelay);
+        //this.setRespawnEvent(this.respawnDelay);
 
         return matches;
     }
