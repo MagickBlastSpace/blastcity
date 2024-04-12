8a27d7afdb292c9ab50a2117b7ff2641e3c3e2e0
BlindBandit <georgynosar@gmail.com>
2024-04-12 16:56:18 +0700

    Added particles and tween effects for UI tile

--- assets/scripts/ui/UITile.ts
+++ assets/scripts/ui/UITile.ts
@@ -1,8 +1,11 @@
-import { _decorator, Component, Node, tween, Vec3, Vec2 } from 'cc';
+import { _decorator, Component, Node, tween, Vec3, Vec2, ParticleSystem2D } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('UITile')
 export class UITile extends Component {
+
+    @property(ParticleSystem2D)
+    particles: ParticleSystem2D = null;
 
     private fallTime: number = 0.35;
     private destroyTime: number = 0.25;
@@ -21,8 +24,12 @@ export class UITile extends Component {
         this.currentY = posY;
 
         tween(this.node)
+            .to(0.05, { scale: new Vec3(0.92, 1.08, 1) }, { easing: 'linear' })
             .to(this.fallTime, { position: new Vec3(posX, posY, 0) })
             .call(() => layout.addChild(this.node))
+            .to(0.07, { scale: new Vec3(1.03, 0.97, 1) }, { easing: 'elasticInOut' })
+            .to(0.07, { scale: new Vec3(0.97, 1.03, 1) }, { easing: 'elasticInOut' })
+            .to(0.07, { scale: new Vec3(1, 1, 1) }, { easing: 'elasticInOut' })
             .start();
 
         this.isBlocked = false;
@@ -44,8 +51,12 @@ export class UITile extends Component {
         tween(this.node).stop();
 
         tween(this.node)
+            .to(0.05, { scale: new Vec3(0.92, 1.08, 1) }, { easing: 'linear' })
             .to(this.fallTime, { position: new Vec3(posX, posY, 0) })
             .call(() => layout.addChild(this.node))
+            .to(0.07, { scale: new Vec3(1.03, 0.97, 1) }, { easing: 'elasticInOut' })
+            .to(0.07, { scale: new Vec3(0.97, 1.03, 1) }, { easing: 'elasticInOut' })
+            .to(0.07, { scale: new Vec3(1, 1, 1) }, { easing: 'elasticInOut' })
             .start();
     }
 
@@ -53,11 +64,15 @@ export class UITile extends Component {
     destroyTile(delay: number) {
         this.isBlocked = true;
 
+        if (this.particles) {
+            this.particles.resetSystem();
+        }
+
         this.scheduleOnce(() => {
             tween(this.node).stop();
 
             tween(this.node)
-                .to(this.destroyTime, { scale: new Vec3(1.8, 1.8, 1.8) }, { easing: 'linear' })
+                .to(this.destroyTime, { scale: new Vec3(1.2, 1.2, 1.2) }, { easing: 'linear' })
                 .call(() => this.node.destroy())
                 .start();
         }, delay);
