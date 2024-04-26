7cf4850aaddf4349411a396885b44a47d0319eb1
BlindBandit <georgynosar@gmail.com>
2024-04-26 15:32:21 +0700

    Removed levels cycle

--- assets/scripts/data/UserData.ts
+++ assets/scripts/data/UserData.ts
@@ -57,9 +57,9 @@ export class UserData extends Component {
 
 
     addProgress() {
-        //if(this.currentProgress < this.levelsCount - 1) {
-        this.currentProgress++;
-        //}
+        if(this.currentProgress < this.levelsCount - 1) {
+            this.currentProgress++;
+        }
 
         SaveData.instance.saveUserData();
     }
