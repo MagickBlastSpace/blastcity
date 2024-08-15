065f55eeb8cd16fbb255762e1fd037e8bd0c1538
BlindBandit <georgynosar@gmail.com>
2024-08-15 17:45:51 +0700

    Added loading frames

--- assets/scripts/ui/loading/UIAssetsLoadingFrame.ts
+++ assets/scripts/ui/loading/UIAssetsLoadingFrame.ts
@@ -1,11 +1,36 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, SpriteFrame, Sprite } from 'cc';
 import { AssetsLoader } from '../../utils/AssetsLoader';
 import { UIFrameBase } from '../UIFrameBase';
 const { ccclass, property } = _decorator;
 
 @ccclass('UIAssetsLoadingFrame')
 export class UIAssetsLoadingFrame extends UIFrameBase {
+
+    @property([SpriteFrame])
+    screens: SpriteFrame[] = [];
+
+    @property(Sprite)
+    screen: Sprite = null;
+
+
     start() {}
+
+    show() {
+        super.show();
+
+        this.setRandomLoadingScreen();
+    }
+
+
+    setRandomLoadingScreen() {
+        if (this.screens.length > 0) {
+            const randomIndex = Math.floor(Math.random() * this.screens.length);
+    
+            this.screen.spriteFrame = this.screens[randomIndex];
+        } else {
+            console.log("Screens array is empty. Cannot set random loading screen.");
+        }
+    }
 }
 
 
