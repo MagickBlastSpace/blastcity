6e9b69826b91597aaaa06124abc2b7d6f40371f3
BlindBandit <georgynosar@gmail.com>
2024-08-19 16:56:52 +0700

    Gamepush initialization await

--- assets/scripts/ui/loading/UILoadingFrame.ts
+++ assets/scripts/ui/loading/UILoadingFrame.ts
@@ -1,3 +1,5 @@
+declare const gamepush: any;
+
 import { _decorator, Component, Node, ProgressBar, director, SceneAsset } from 'cc';
 const { ccclass, property } = _decorator;
 
@@ -7,14 +9,44 @@ export class UILoadingFrame extends Component {
     @property(ProgressBar)
     loadingBar: ProgressBar = null;
 
+    private timeCooldown: number = 1.5;
+
+
     start() {
+        this.initializeGamePush();
+    }
+
+
+    initializeGamePush() {
+        try {
+            if(gamepush.player.ready) {
+                console.log("GamePush module is ready!");
+    
+                this.loadScene();
+            }
+            else {
+                this.scheduleNewTry();
+            }
+        } catch (error) {
+            console.error("Error initializing GamePush:", error);
+
+            this.scheduleNewTry();
+        }
+    }
+
+    loadScene() {
         director.preloadScene("scene", this.onProgressLoadScene, () => {
             director.loadScene("scene");
         })
     }
 
-
     private onProgressLoadScene = (completedCount: number, totalCount: number, item: any) => {
         this.loadingBar.progress = completedCount / totalCount;
+    }
+
+    scheduleNewTry() {
+        this.scheduleOnce(() => {
+            this.initializeGamePush();
+        }, this.timeCooldown);
     }
 }
