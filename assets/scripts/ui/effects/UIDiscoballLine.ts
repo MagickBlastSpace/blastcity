3268ac76c3494f591b5ae51dde77f1bc3f77395e
BlindBandit <georgynosar@gmail.com>
2024-08-20 18:17:19 +0700

    Discoball line effect update

--- assets/scripts/ui/effects/UIDiscoballLine.ts
+++ assets/scripts/ui/effects/UIDiscoballLine.ts
@@ -11,15 +11,26 @@ export class UIDiscoballLine extends Component {
     setLinePositions(startPos: Vec3, endPos: Vec3) {
         let direction = new Vec3();
         Vec3.subtract(direction, endPos, startPos);
-
+    
         let distance = direction.length();
-        this.line.node.width = distance;
-
         let midPoint = startPos.clone().add(direction.multiplyScalar(0.5));
-        this.line.node.setPosition(midPoint);
-
+    
         let angle = Math.atan2(direction.y, direction.x) * (180 / Math.PI);
         this.line.node.angle = angle;
+    
+        this.flyLine(startPos, endPos, distance);
+    }
+    
+    flyLine(startPos: Vec3, endPos: Vec3, maxDistance: number) {
+        const flyDuration = 0.67;
+    
+        this.line.node.width = 0;
+        this.line.node.setPosition(startPos);
+    
+        cc.tween(this.line.node)
+            .to(flyDuration / 2, { width: maxDistance, position: startPos.clone().add(endPos).multiplyScalar(0.5) })
+            .to(flyDuration / 2, { width: 0, position: endPos })
+            .start();
     }
 }
 
