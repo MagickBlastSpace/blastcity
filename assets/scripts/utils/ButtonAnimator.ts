8d599e62f3f49bf1a4a1eb0f5cf75efb63507d57
BlindBandit <georgynosar@gmail.com>
2024-09-23 21:21:14 +0700

    Button animator fix

--- assets/scripts/utils/ButtonAnimator.ts
+++ assets/scripts/utils/ButtonAnimator.ts
@@ -4,6 +4,9 @@ const { ccclass, property } = _decorator;
 @ccclass('ButtonAnimator')
 export class ButtonAnimator extends Component {
 
+    private startScale: Vec3;
+
+
     start() {
         const button = this.node.getComponent(Button);
 
@@ -12,18 +15,22 @@ export class ButtonAnimator extends Component {
             this.node.on(Node.EventType.TOUCH_END, this.onButtonReleased, this);
             this.node.on(Node.EventType.TOUCH_CANCEL, this.onButtonReleased, this);
         }
+
+        this.startScale = new Vec3(this.node.scale.x, this.node.scale.y, this.node.scale.z);
     }
 
     onButtonPressed() {
+        this.startScale = new Vec3(this.node.scale.x, this.node.scale.y, this.node.scale.z);
+
         tween(this.node)
-            .to(0.2, { scale: new Vec3(1.1, 1.1, 1) })
+            .to(0.2, { scale: new Vec3(this.startScale.x * 1.1, this.startScale.y * 1.1, this.startScale.z) })
             .start();
     }
 
     onButtonReleased() {
         tween(this.node)
-            .to(0.2, { scale: new Vec3(0.9, 0.9, 1) })
-            .to(0.2, { scale: new Vec3(1, 1, 1) })
+            .to(0.2, { scale: new Vec3(this.startScale.x * 0.9, this.startScale.y * 0.9, this.startScale.z) })
+            .to(0.2, { scale: new Vec3(this.startScale.x, this.startScale.y, this.startScale.z) })
             .start();
     }
 }
