6463edaa5657fde945bf8df4fb93f61826384c14
BlindBandit <georgynosar@gmail.com>
2024-11-14 20:14:50 +0700

    Lightning and Skyrace update

--- assets/scripts/ui/events/Lightning/UIEventLightning.ts
+++ assets/scripts/ui/events/Lightning/UIEventLightning.ts
@@ -73,7 +73,7 @@ export class UIEventLightning extends UIEventPopupFrameBase {
         this.isEventStarted = this.eventController.getIsStarted();
         this.isEventComplete = this.eventController.getIsComplete();
 
-        this.playersLayout.active = this.isEventStarted && !this.isEventComplete;
+        this.playersLayout.active = this.isEventStarted || this.isEventComplete;
         this.rewardLayout.active = this.isEventComplete;
    
         this.startBtn.node.active = this.eventController.canParticipate() && !this.isEventStarted && !this.isEventComplete;
---
+++ assets/scripts/ui/events/Lightning/UIEventLightningPlayerItem.ts
@@ -0,0 +1,52 @@
+import { _decorator, Component, Node, Button } from 'cc';
+import { UILeaderboardFriendItem } from '../../leaderboard/UILeaderboardFriendItem';
+import { PlayerEventData } from '../../../data/EventData';
+const { ccclass, property } = _decorator;
+
+@ccclass('UIEventLightningPlayerItem')
+export class UIEventLightningPlayerItem extends UILeaderboardFriendItem {
+
+    @property(Button)
+    infoRewardBtn: Button = null;
+    @property(Button)
+    closeInfoRewardBtn: Button = null;
+
+    @property(Node)
+    infoReward: Node = null;
+
+
+    start() {
+        super.start();
+
+        if(this.infoRewardBtn && this.infoRewardBtn !== undefined) {
+            this.infoRewardBtn.node.on(Button.EventType.CLICK, this.onInfoRewardClick, this);
+        }
+
+        if(this.closeInfoRewardBtn && this.closeInfoRewardBtn !== undefined) {
+            this.closeInfoRewardBtn.node.on(Button.EventType.CLICK, this.onCloseInfoRewardClick, this);
+        }
+    }
+
+    refresh(data: PlayerEventData) {
+        super.refresh(data);
+
+        if(this.rewardsLayout && this.rewardsLayout !== undefined) {
+            this.rewardsLayout.active = this.index === 0;
+
+            if(this.rewardIcon && this.index === 0) {
+                this.rewardIcon.spriteFrame = this.rewardIcons[this.index];
+            }
+        }
+    }
+
+
+    onInfoRewardClick() {
+        this.infoReward.active = true;
+    }
+
+    onCloseInfoRewardClick() {
+        this.infoReward.active = false;
+    }
+}
+
+
