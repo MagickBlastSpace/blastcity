6cc1bcdf5e5daf28795c40d991ee09bb11c474cd
BlindBandit <georgynosar@gmail.com>
2025-03-03 18:44:41 +0700

    Clan create fix

--- assets/scripts/ui/clans/UIClansCreateFrame.ts
+++ assets/scripts/ui/clans/UIClansCreateFrame.ts
@@ -5,6 +5,7 @@ const { ccclass, property } = _decorator;
 @ccclass('UIClansCreateFrame')
 export class UIClansCreateFrame extends UIPopupFrameBase {
 
+    /*landscape*/
     @property(Button)
     createBtn: Button = null;
 
@@ -19,6 +20,21 @@ export class UIClansCreateFrame extends UIPopupFrameBase {
     @property(Label)
     privacy: Label = null;
 
+    /*portrait*/
+    @property(Button)
+    createBtn_portrait: Button = null;
+
+    @property(Button)
+    rightPrivateBtn_portrait: Button = null;
+    @property(Button)
+    leftPrivateBtn_portrait: Button = null;
+
+    @property(EditBox)
+    nameInput_portrait: EditBox = null;
+
+    @property(Label)
+    privacy_portrait: Label = null;
+
     private isPrivate: boolean = false;
 
 
@@ -30,6 +46,11 @@ export class UIClansCreateFrame extends UIPopupFrameBase {
 
         this.rightPrivateBtn.node.on(Button.EventType.CLICK, this.changePrivacy, this);
         this.leftPrivateBtn.node.on(Button.EventType.CLICK, this.changePrivacy, this);
+
+        this.createBtn_portrait.node.on(Button.EventType.CLICK, this.onCreateBtnClick_portrait, this);
+
+        this.rightPrivateBtn_portrait.node.on(Button.EventType.CLICK, this.changePrivacy, this);
+        this.leftPrivateBtn_portrait.node.on(Button.EventType.CLICK, this.changePrivacy, this);
     }
 
     onCreateBtnClick() {
@@ -40,11 +61,20 @@ export class UIClansCreateFrame extends UIPopupFrameBase {
         this.node.emit("create", this.nameInput.string, this.isPrivate);
     }
 
+    onCreateBtnClick_portrait() {
+        if(this.nameInput_portrait.string === "") {
+            return;
+        }
+
+        this.node.emit("create", this.nameInput_portrait.string, this.isPrivate);
+    }
+
 
     changePrivacy() {
         this.isPrivate = !this.isPrivate;
 
         this.privacy.string = this.isPrivate ? "Private" : "Open";
+        this.privacy_portrait.string = this.isPrivate ? "Private" : "Open";
     }
 }
 
