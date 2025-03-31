4abbb22a2ae828a140a706b7c46ff6875a26a01f
BlindBandit <georgynosar@gmail.com>
2025-03-31 19:24:07 +0700

    prem bug fixed

--- assets/scripts/game/events/BattlepassEvent.ts
+++ assets/scripts/game/events/BattlepassEvent.ts
@@ -263,6 +263,8 @@ export class BattlepassEvent extends RocketFeverEvent {
     saveUnpickedRewards() {
         if (this.isRewardAvailable()) {
             let totalReward = new EventRewardData();
+
+            let isPrem = UserData.instance.getIsPremium();
     
             for (let i = 0; i < this.currentStage; i++) {
                 if (!this.takenRewards.includes(i)) {
@@ -288,27 +290,29 @@ export class BattlepassEvent extends RocketFeverEvent {
                     totalReward.isChest ||= reward.isChest;
                 }
 
-                if (!this.takenRewards_Premium.includes(i)) {
-                    let reward = this.eventData[i]?.rewards[1];
-
-                    totalReward.gold += reward.gold;
-                    totalReward.progress += reward.progress;
-                    totalReward.startBonus_Bomb += reward.startBonus_Bomb;
-                    totalReward.startBonus_Rocket += reward.startBonus_Rocket;
-                    totalReward.startBonus_Discoball += reward.startBonus_Discoball;
-                    totalReward.booster_Hammer += reward.booster_Hammer;
-                    totalReward.booster_Bow += reward.booster_Bow;
-                    totalReward.booster_Cannon += reward.booster_Cannon;
-                    totalReward.booster_Jester += reward.booster_Jester;
-                    totalReward.endlessLives_Minutes += reward.endlessLives_Minutes;
-                    totalReward.modifierX2_Minutes += reward.modifierX2_Minutes;
-                    totalReward.bomb_Minutes += reward.bomb_Minutes;
-                    totalReward.rocket_Minutes += reward.rocket_Minutes;
-                    totalReward.discoball_Minutes += reward.discoball_Minutes;
-                    totalReward.battlepass += reward.battlepass;
-                    totalReward.cardsPack += reward.cardsPack;
-                    totalReward.cards.push(...reward.cards);
-                    totalReward.isChest ||= reward.isChest;
+                if(isPrem) {
+                    if (!this.takenRewards_Premium.includes(i)) {
+                        let reward = this.eventData[i]?.rewards[1];
+    
+                        totalReward.gold += reward.gold;
+                        totalReward.progress += reward.progress;
+                        totalReward.startBonus_Bomb += reward.startBonus_Bomb;
+                        totalReward.startBonus_Rocket += reward.startBonus_Rocket;
+                        totalReward.startBonus_Discoball += reward.startBonus_Discoball;
+                        totalReward.booster_Hammer += reward.booster_Hammer;
+                        totalReward.booster_Bow += reward.booster_Bow;
+                        totalReward.booster_Cannon += reward.booster_Cannon;
+                        totalReward.booster_Jester += reward.booster_Jester;
+                        totalReward.endlessLives_Minutes += reward.endlessLives_Minutes;
+                        totalReward.modifierX2_Minutes += reward.modifierX2_Minutes;
+                        totalReward.bomb_Minutes += reward.bomb_Minutes;
+                        totalReward.rocket_Minutes += reward.rocket_Minutes;
+                        totalReward.discoball_Minutes += reward.discoball_Minutes;
+                        totalReward.battlepass += reward.battlepass;
+                        totalReward.cardsPack += reward.cardsPack;
+                        totalReward.cards.push(...reward.cards);
+                        totalReward.isChest ||= reward.isChest;
+                    }
                 }
             }
 
