f3dc5a1fdd0038f8758cbb97bb5927dd4bf65a6f
BlindBandit <georgynosar@gmail.com>
2025-03-31 19:22:11 +0700

    Real vk shop added

--- assets/scripts/data/GameData.ts
+++ assets/scripts/data/GameData.ts
@@ -81,6 +81,9 @@ export class MovesShopStageData {
 @ccclass('ShopItemData')
 export class ShopItemData {
     @property
+    tag = "";
+    
+    @property
     gold = 0;
 
     @property
--- assets/scripts/game/Shop.ts
+++ assets/scripts/game/Shop.ts
@@ -1,3 +1,5 @@
+declare const gamepush: any;
+
 import { _decorator, Component, Node } from 'cc';
 import { ShopItemData } from '../data/GameData';
 import { UserData } from '../data/UserData';
@@ -5,7 +7,28 @@ const { ccclass, property } = _decorator;
 
 @ccclass('Shop')
 export class Shop extends Component {
-    buy(data: ShopItemData) {
+
+    async buy(data: ShopItemData) {
+        if (gamepush.payments.isAvailable) {
+            console.log("payments available: " + data.tag);
+
+            await gamepush.payments.purchase({ tag: data.tag });
+
+            this.consume(data);
+
+            await gamepush.player.sync();
+
+            await gamepush.payments.consume({ tag: data.tag });
+
+            console.log("payment successfull: " + data.tag);
+        }
+        else {
+            console.log("payments not available");
+        }
+    }
+
+
+    consume(data: ShopItemData) {
         UserData.instance.addResource("gold", data.gold);
 
         UserData.instance.addResource("bomb", data.startBonus_Bomb);
