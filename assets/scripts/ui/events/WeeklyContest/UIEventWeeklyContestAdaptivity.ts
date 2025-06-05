0c8a2266900701c5bf7b6903afd2020c711b914c
BlindBandit <georgynosar@gmail.com>
2025-06-05 18:54:18 +0700

    weekly contest desktop adaptivity

--- assets/scripts/ui/events/WeeklyContest/UIEventWeeklyContestAdaptivity.ts
+++ assets/scripts/ui/events/WeeklyContest/UIEventWeeklyContestAdaptivity.ts
@@ -48,77 +48,15 @@ export class UIEventWeeklyContestAdaptivity extends UIAdaptivityBase {
         let h = visibleSize.height;
     
         if (w > h) {
-            const x = 0.140 * w;
-
-            const banner_H = 1.867 * x;
-            const banner_W = 5.0133 * x;
-            this.banner.getComponent(UITransform).setContentSize(new Size(banner_W, banner_H))
-
-            const bannerPaddingTop = 1.427 * x;
-            this.banner_Widget.top = bannerPaddingTop;
-
-            const scroll_H = h - (1.427 + 0.033 + 1.867 + 0.3 + 0.7867) * x;
-            const scroll_W = 5.0133 * x;
-            this.scroll.getComponent(UITransform).setContentSize(new Size(scroll_W, scroll_H));
-
-            const scrollPaddingTop = (1.427 + 0.033 + 1.867) * x;
-            this.scroll_Widget.top = scrollPaddingTop;
-
-            const layoutSize = 1.3 * x;
-            const layoutScale = layoutSize / this.basic_prize_layout_size_H;
-            this.prizeLayout.node.setScale(new Vec3(layoutScale, layoutScale, 1));
-
-            const headerSize = 2.3733 * x;
-            const headerScale = headerSize / this.basic_header_size;
-            this.header.setScale(new Vec3(headerScale, headerScale, 1));
-
-            const infoSize = 0.18 * x;
-            const infoScale = infoSize / this.basic_info_size;
-            this.btnInfo.setScale(new Vec3(infoScale, infoScale, 1));
-
+            const ratio = w / h;
+            if(ratio > 1.5) {
+                this.makeDesktopVariation(w, h);
+            }
+            else {
+                this.makeTabletVariation(w, h);
+            }
         } else {
-            const x = 0.262 * w;
-
-            const banner_H = 2.276 * x;
-            const banner_W = 3.596 * x;
-            this.banner.getComponent(UITransform).setContentSize(new Size(banner_W, banner_H))
-
-            /*const back_prize_W = 1.0256 * x;
-            
-            const back_gold_H = 1.481 * x;
-            this.back_gold.getComponent(UITransform).setContentSize(new Size(back_prize_W, back_gold_H));
-
-            const back_silver_H = 1.327 * x;
-            this.back_silver.getComponent(UITransform).setContentSize(new Size(back_prize_W, back_silver_H));
-
-            const back_bronze_H = 1.212 * x;
-            this.back_bronze.getComponent(UITransform).setContentSize(new Size(back_prize_W, back_bronze_H));*/
-
-            const bannerPaddingTop = (1.468 + 0.16) * x;
-            this.banner_Widget.top = bannerPaddingTop;
-
-            const scroll_H = h - (1.468 + 0.16 + 2.276 + 0.064 + 0.8526) * x;
-            const scroll_W = 3.436 * x;
-            this.scroll.getComponent(UITransform).setContentSize(new Size(scroll_W, scroll_H));
-
-            const scrollPaddingTop = (1.468 + 0.16 + 2.276) * x;
-            this.scroll_Widget.top = scrollPaddingTop;
-
-            const layoutSize = 3.244 * x;
-            const layoutScale = layoutSize / this.basic_prize_layout_size;
-            this.prizeLayout.node.setScale(new Vec3(layoutScale, layoutScale, 1));
-
-            const headerSize = 2.1667 * x;
-            const headerScale = headerSize / this.basic_header_size;
-            this.header.setScale(new Vec3(headerScale, headerScale, 1));
-
-            const infoSize = 0.192 * x;
-            const infoScale = infoSize / this.basic_info_size;
-            this.btnInfo.setScale(new Vec3(infoScale, infoScale, 1));
-
-            /*const prizeLayoutSpacing = 0.0962 * x;
-            this.prizeLayout.spacingX = prizeLayoutSpacing;
-            this.prizeLayout.updateLayout();*/
+            this.makeMobileVariation(w, h);
         }
 
         for(let i = 0; i < this.items.length; i++) {
@@ -129,6 +67,112 @@ export class UIEventWeeklyContestAdaptivity extends UIAdaptivityBase {
 
     addItem(item: UILeaderboardItemAdaptivity) {
         this.items.push(item);
+    }
+
+
+    makeMobileVariation(w: number, h: number) {
+        const x = 0.262 * w;
+
+        const banner_H = 2.276 * x;
+        const banner_W = 3.596 * x;
+        this.banner.getComponent(UITransform).setContentSize(new Size(banner_W, banner_H))
+
+        /*const back_prize_W = 1.0256 * x;
+            
+        const back_gold_H = 1.481 * x;
+        this.back_gold.getComponent(UITransform).setContentSize(new Size(back_prize_W, back_gold_H));
+
+        const back_silver_H = 1.327 * x;
+        this.back_silver.getComponent(UITransform).setContentSize(new Size(back_prize_W, back_silver_H));
+
+        const back_bronze_H = 1.212 * x;
+        this.back_bronze.getComponent(UITransform).setContentSize(new Size(back_prize_W, back_bronze_H));*/
+
+        const bannerPaddingTop = (1.468 + 0.16) * x;
+        this.banner_Widget.top = bannerPaddingTop;
+
+        const scroll_H = h - (1.468 + 0.16 + 2.276 + 0.064 + 0.8526) * x;
+        const scroll_W = 3.436 * x;
+        this.scroll.getComponent(UITransform).setContentSize(new Size(scroll_W, scroll_H));
+
+        const scrollPaddingTop = (1.468 + 0.16 + 2.276) * x;
+        this.scroll_Widget.top = scrollPaddingTop;
+
+        const layoutSize = 3.244 * x;
+        const layoutScale = layoutSize / this.basic_prize_layout_size;
+        this.prizeLayout.node.setScale(new Vec3(layoutScale, layoutScale, 1));
+
+        const headerSize = 2.1667 * x;
+        const headerScale = headerSize / this.basic_header_size;
+        this.header.setScale(new Vec3(headerScale, headerScale, 1));
+
+        const infoSize = 0.192 * x;
+        const infoScale = infoSize / this.basic_info_size;
+        this.btnInfo.setScale(new Vec3(infoScale, infoScale, 1));
+
+        /*const prizeLayoutSpacing = 0.0962 * x;
+        this.prizeLayout.spacingX = prizeLayoutSpacing;
+        this.prizeLayout.updateLayout();*/
+    }
+
+    makeDesktopVariation(w: number, h: number) {
+        const x = 0.125 * h;
+
+        const banner_H = h - (1.92 + 1.15 + 2.2 + 0.62) * x;
+        const banner_W = 7.52 * x;
+        this.banner.getComponent(UITransform).setContentSize(new Size(banner_W, banner_H))
+
+        const bannerPaddingTop = (2.8 + 0.62) * x;
+        this.banner_Widget.top = bannerPaddingTop;
+
+        const scroll_H = 1.92 * x;
+        const scroll_W = 7.52 * x;
+        this.scroll.getComponent(UITransform).setContentSize(new Size(scroll_W, scroll_H));
+
+        const scrollPaddingTop = bannerPaddingTop + banner_H + 0.12 * x;
+        this.scroll_Widget.top = scrollPaddingTop;
+
+        const layoutSize = 1.4 * x;
+        const layoutScale = layoutSize / this.basic_prize_layout_size_H;
+        this.prizeLayout.node.setScale(new Vec3(layoutScale, layoutScale, 1));
+
+        const headerSize = 2.8 * x;
+        const headerScale = headerSize / this.basic_header_size;
+        this.header.setScale(new Vec3(headerScale, headerScale, 1));
+
+        const infoSize = 0.27 * x;
+        const infoScale = infoSize / this.basic_info_size;
+        this.btnInfo.setScale(new Vec3(infoScale, infoScale, 1));
+    }
+
+    makeTabletVariation(w: number, h: number) {
+        const x = 0.140 * w;
+
+        const banner_H = 1.867 * x;
+        const banner_W = 5.0133 * x;
+        this.banner.getComponent(UITransform).setContentSize(new Size(banner_W, banner_H))
+
+        const bannerPaddingTop = 1.427 * x;
+        this.banner_Widget.top = bannerPaddingTop;
+
+        const scroll_H = h - (1.427 + 0.033 + 1.867 + 0.3 + 0.7867) * x;
+        const scroll_W = 5.0133 * x;
+        this.scroll.getComponent(UITransform).setContentSize(new Size(scroll_W, scroll_H));
+
+        const scrollPaddingTop = (1.427 + 0.033 + 1.867) * x;
+        this.scroll_Widget.top = scrollPaddingTop;
+
+        const layoutSize = 1.3 * x;
+        const layoutScale = layoutSize / this.basic_prize_layout_size_H;
+        this.prizeLayout.node.setScale(new Vec3(layoutScale, layoutScale, 1));
+
+        const headerSize = 2.3733 * x;
+        const headerScale = headerSize / this.basic_header_size;
+        this.header.setScale(new Vec3(headerScale, headerScale, 1));
+
+        const infoSize = 0.18 * x;
+        const infoScale = infoSize / this.basic_info_size;
+        this.btnInfo.setScale(new Vec3(infoScale, infoScale, 1));
     }
 }
 
--- assets/scripts/ui/leaderboard/adaptivity/UILeaderboardItemAdaptivity.ts
+++ assets/scripts/ui/leaderboard/adaptivity/UILeaderboardItemAdaptivity.ts
@@ -29,41 +29,81 @@ export class UILeaderboardItemAdaptivity extends UIAdaptivityBase {
         let h = visibleSize.height;
     
         if (w > h) {   
-            const x = 0.140 * w;
-
-            const container_H = 0.567 * x;
-            const container_W = 4.88 * x;
-            this.container.getComponent(UITransform).setContentSize(new Size(container_W, container_H));
-
-            const itemScale = container_H / this.basic_H;
-            for(let i = 0; i < this.items.length; i++) {
-                this.items[i].setScale(new Vec3(itemScale, itemScale, 1));
-            }
-
-            const sidePadding = 0.133 * x;
-            this.number_Widget.left = sidePadding;
-            this.avatar_Widget.left = sidePadding + 0.4 * x;
-            this.player_Widget.left = sidePadding + 0.9 * x;
-            this.progress_Widget.right = sidePadding;
+            
 
         } else {
-            const x = 0.262 * w;
-
-            const container_H = 0.5769 * x;
-            const container_W = 3.436 * x;
-            this.container.getComponent(UITransform).setContentSize(new Size(container_W, container_H));
-
-            const itemScale = container_H / this.basic_H;
-            for(let i = 0; i < this.items.length; i++) {
-                this.items[i].setScale(new Vec3(itemScale, itemScale, 1));
-            }
-
-            const sidePadding = 0.141 * x;
-            this.number_Widget.left = sidePadding;
-            this.avatar_Widget.left = sidePadding + 0.3 * x;
-            this.player_Widget.left = sidePadding + 0.9 * x;
-            this.progress_Widget.right = sidePadding;
+            
         }
+
+        if (w > h) {
+            const ratio = w / h;
+            if(ratio > 1.5) {
+                this.makeDesktopVariation(w, h);
+            }
+            else {
+                this.makeTabletVariation(w, h);
+            }
+        } else {
+            this.makeMobileVariation(w, h);
+        }
+    }
+
+
+    makeDesktopVariation(w: number, h: number) {
+        const x = 0.125 * h;
+
+        const container_H = 0.85 * x;
+        const container_W = 7.32 * x;
+        this.container.getComponent(UITransform).setContentSize(new Size(container_W, container_H));
+
+        const itemScale = container_H / this.basic_H;
+        for(let i = 0; i < this.items.length; i++) {
+            this.items[i].setScale(new Vec3(itemScale, itemScale, 1));
+        }
+
+        const sidePadding = 0.2 * x;
+        this.number_Widget.left = sidePadding;
+        this.avatar_Widget.left = sidePadding + 0.4 * x;
+        this.player_Widget.left = sidePadding + 0.9 * x;
+        this.progress_Widget.right = sidePadding;
+    }
+
+    makeTabletVariation(w: number, h: number) {
+        const x = 0.140 * w;
+
+        const container_H = 0.567 * x;
+        const container_W = 4.88 * x;
+        this.container.getComponent(UITransform).setContentSize(new Size(container_W, container_H));
+
+        const itemScale = container_H / this.basic_H;
+        for(let i = 0; i < this.items.length; i++) {
+            this.items[i].setScale(new Vec3(itemScale, itemScale, 1));
+        }
+
+        const sidePadding = 0.133 * x;
+        this.number_Widget.left = sidePadding;
+        this.avatar_Widget.left = sidePadding + 0.4 * x;
+        this.player_Widget.left = sidePadding + 0.9 * x;
+        this.progress_Widget.right = sidePadding;
+    }
+
+    makeMobileVariation(w: number, h: number) {
+        const x = 0.262 * w;
+
+        const container_H = 0.5769 * x;
+        const container_W = 3.436 * x;
+        this.container.getComponent(UITransform).setContentSize(new Size(container_W, container_H));
+
+        const itemScale = container_H / this.basic_H;
+        for(let i = 0; i < this.items.length; i++) {
+            this.items[i].setScale(new Vec3(itemScale, itemScale, 1));
+        }
+
+        const sidePadding = 0.141 * x;
+        this.number_Widget.left = sidePadding;
+        this.avatar_Widget.left = sidePadding + 0.3 * x;
+        this.player_Widget.left = sidePadding + 0.9 * x;
+        this.progress_Widget.right = sidePadding;
     }
 }
 
