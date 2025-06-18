cb5a76c9980d38c61a351ad37299070c1e0216d6
BlindBandit <georgynosar@gmail.com>
2025-06-18 13:48:23 +0700

    Collection frame adaptivity

--- assets/scripts/ui/collection/adaptivity/UICollectionFrameAdaptivity.ts
+++ assets/scripts/ui/collection/adaptivity/UICollectionFrameAdaptivity.ts
@@ -21,7 +21,16 @@ export class UICollectionFrameAdaptivity extends UIAdaptivityBase {
     @property(Node)
     progressContainer: Node = null;
     @property(Node)
+    progressRight: Node = null;
+    @property(Node)
+    progressLeft: Node = null;
+    @property(Node)
     scroll: Node = null;
+
+    @property(Node)
+    btnInfo: Node = null;
+    @property(Node)
+    btnChest: Node = null;
 
     @property(Widget)
     header_Widget: Widget = null;
@@ -38,9 +47,19 @@ export class UICollectionFrameAdaptivity extends UIAdaptivityBase {
     @property([UICollectionItemAdaptivity])
     items: UICollectionItemAdaptivity[] = [];
 
+    @property([Node])
+    landscapeNodes: Node[] = [];
+
     private basic_header_H: number = 153;
     private basic_banner_H: number = 642;
     private basic_progress_H: number = 620;
+
+    private basic_btnInfo_size: number = 127;
+    private basic_btnChest_size: number = 176;
+
+    private basic_progress_right_W: number = 1212;
+    private basic_progress_left_W: number = 1184;
+    private basic_progress_left_W_ls: number = 1857;
 
     private mobileScaleMul: number = 0.5;
 
@@ -71,6 +90,10 @@ export class UICollectionFrameAdaptivity extends UIAdaptivityBase {
     makeDesktopVariation(w: number, h: number) {
         this.bannerImg.spriteFrame = this.bannerLandscape;
 
+        for(let i = 0; i < this.landscapeNodes.length; i++) {
+            this.landscapeNodes[i].active = true;
+        }
+
         const x = 0.1294 * h;
 
         const banner_H = 1.5 * x;
@@ -87,6 +110,26 @@ export class UICollectionFrameAdaptivity extends UIAdaptivityBase {
         const progress_Scale = progress_H / this.basic_progress_H;
 
         this.progressContainer.setScale(new Vec3(progress_Scale, progress_Scale, 1));
+
+        let progress_r_W = 2.6635 * x;
+        const progressRight_Scale = progress_r_W / this.basic_progress_right_W;
+
+        this.progressRight.setScale(new Vec3(progressRight_Scale / progress_Scale, progressRight_Scale / progress_Scale, 1));
+
+        let progress_l_W = 4.0829 * x;
+        const progressLeft_Scale = progress_l_W / this.basic_progress_left_W_ls;
+
+        this.progressLeft.setScale(new Vec3(progressLeft_Scale / progress_Scale, progressLeft_Scale / progress_Scale, 1));
+
+        const spacingX = 1250;
+
+        const halfWidthL = (progress_l_W / 2);
+        const halfWidthR = (progress_r_W / 2);
+
+        const totalWidth = halfWidthL + halfWidthR + spacingX;
+
+        this.progressLeft.setPosition(new Vec3(-totalWidth / 2 + halfWidthL, 0, 0));
+        this.progressRight.setPosition(new Vec3(totalWidth / 2 - halfWidthR, 0, 0));
 
         const headerPadding = (1.5 - 0.23) * x;
 
@@ -118,6 +161,16 @@ export class UICollectionFrameAdaptivity extends UIAdaptivityBase {
 
         this.banner_Widget.left = sidePadding;
         this.banner_Widget.right = sidePadding;
+
+        let btnInfo_size = 0.5192 * x;
+        const btnInfoScale = btnInfo_size / this.basic_btnInfo_size;
+
+        this.btnInfo.setScale(new Vec3(btnInfoScale / banner_Scale, btnInfoScale / banner_Scale, 1));
+
+        let btnChest_size = 0.5192 * x;
+        const btnChestScale = btnChest_size / this.basic_btnChest_size;
+
+        this.btnChest.setScale(new Vec3(btnChestScale / banner_Scale, btnChestScale / banner_Scale, 1));
     }
 
     makeTabletVariation(w: number, h: number) {
@@ -126,6 +179,10 @@ export class UICollectionFrameAdaptivity extends UIAdaptivityBase {
 
     makeMobileVariation(w: number, h: number) {
         this.bannerImg.spriteFrame = this.bannerPortrait;
+
+        for(let i = 0; i < this.landscapeNodes.length; i++) {
+            this.landscapeNodes[i].active = false;
+        }
 
         const x = 0.30626 * w * this.mobileScaleMul;
 
@@ -143,6 +200,22 @@ export class UICollectionFrameAdaptivity extends UIAdaptivityBase {
         const progress_Scale = progress_H / this.basic_progress_H;
 
         this.progressContainer.setScale(new Vec3(progress_Scale, progress_Scale, 1));
+
+        const progress_r_W = 3.1436 * x * this.mobileScaleMul;
+        const progressRight_Scale = progress_r_W / this.basic_progress_right_W;
+
+        this.progressRight.setScale(new Vec3(progressRight_Scale, progressRight_Scale, 1));
+
+        const progress_l_W = 2.5635 * x * this.mobileScaleMul;
+        const progressLeft_Scale = progress_l_W / this.basic_progress_left_W;
+
+        this.progressLeft.setScale(new Vec3(progressLeft_Scale, progressLeft_Scale, 1));
+
+        this.progressRight.setPosition(new Vec3(0, 72, 0));
+        this.progressLeft.setPosition(new Vec3(0, -207, 0));
+
+        /*this.progressRight_Widget.bottom = 217;
+        this.progressLeft_Widget.bottom = 52;*/
 
         const headerPadding = (1.3425 - 0.165) * x;
 
@@ -172,6 +245,16 @@ export class UICollectionFrameAdaptivity extends UIAdaptivityBase {
 
         this.banner_Widget.left = 0;
         this.banner_Widget.right = 0;
+
+        let btnInfo_size = 0.2762 * x * this.mobileScaleMul;
+        const btnInfoScale = btnInfo_size / this.basic_btnInfo_size;
+
+        this.btnInfo.setScale(new Vec3(btnInfoScale / banner_Scale, btnInfoScale / banner_Scale, 1));
+
+        let btnChest_size = 0.4805 * x * this.mobileScaleMul;
+        const btnChestScale = btnChest_size / this.basic_btnChest_size;
+
+        this.btnChest.setScale(new Vec3(btnChestScale / banner_Scale, btnChestScale / banner_Scale, 1));
     }
 
 
