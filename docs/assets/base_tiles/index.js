ff44c000d95a52d53dd56fa894565d542b1a43e6
Sneganna Tumanova <Sneganna.snow@mail.ru>
2026-08-26 14:49:39 +0300

    chore: add web build for GitHub Pages

---
+++ docs/assets/base_tiles/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/base_tiles",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/base_tiles', 'chunks:///_virtual/base_tiles'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
