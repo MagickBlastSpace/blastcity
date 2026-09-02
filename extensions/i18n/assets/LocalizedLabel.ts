1484ae0f4b6326e69c75ad4b4ee8cb47851a8754
Sneganna Tumanova <Sneganna.snow@mail.ru>
2026-09-02 21:24:13 +0300

    Fix localization initialization

--- extensions/i18n/assets/LocalizedLabel.ts
+++ extensions/i18n/assets/LocalizedLabel.ts
@@ -1,4 +1,3 @@
-
 import * as i18n from './LanguageData';
 
 import { _decorator, Component, Label } from 'cc';
@@ -7,37 +6,37 @@ const { ccclass, property, executeInEditMode } = _decorator;
 @ccclass('LocalizedLabel')
 @executeInEditMode
 export class LocalizedLabel extends Component {
-    label: Label | null = null;
+	label: Label | null = null;
 
-    @property({ visible: false })
-    key: string = '';
+	@property({ visible: false })
+	key: string = '';
 
-    @property({ displayName: 'Key', visible: true })
-    get _key() {
-        return this.key;
-    }
-    set _key(str: string) {
-        this.updateLabel();
-        this.key = str;
-    }
+	@property({ displayName: 'Key', visible: true })
+	get _key() {
+		return this.key;
+	}
+	set _key(str: string) {
+		this.updateLabel();
+		this.key = str;
+	}
 
-    onLoad() {
-        if (!i18n.ready) {
-            i18n.init('zh');
-        }
-        this.fetchRender();
-    }
+	onLoad() {
+		if (!i18n.ready) {
+			i18n.init('ru');
+		}
+		this.fetchRender();
+	}
 
-    fetchRender () {
-        let label = this.getComponent('cc.Label') as Label;
-        if (label) {
-            this.label = label;
-            this.updateLabel();
-            return;
-        } 
-    }
+	fetchRender() {
+		let label = this.getComponent('cc.Label') as Label;
+		if (label) {
+			this.label = label;
+			this.updateLabel();
+			return;
+		}
+	}
 
-    updateLabel () {
-        this.label && (this.label.string = i18n.t(this.key));
-    }
+	updateLabel() {
+		this.label && (this.label.string = i18n.t(this.key));
+	}
 }
